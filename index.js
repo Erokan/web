7bddedc6e53ee7b145f1dd464020a8b1fba5e715
Erokan Canbazoglu <erokancanbazoglu@Erokan-MacBook-Pro.local>
2020-02-20 16:35:50 +0300

    final

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-var isim = "Erokan";
-console.log("Merhaba benim adım :" + isim);
+const colors = require('colors');
 
-function toplama(sayi1, sayi2, sayi3){
-    return sayi1 + sayi2
-}
-
-console.log(toplama(10,20,30));
-console.log(toplama(100,200));
-
+var isim = "Erokan"
+console.log(isim.blue);
