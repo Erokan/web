c8428b42f34e425341a838a14dd92b7e1acab2cc
Erokan Canbazoglu <erokancanbazoglu@Erokan-MacBook-Pro.local>
2020-02-20 16:28:15 +0300

    js added

---
+++ index.js
@@ -0,0 +1,10 @@
+var isim = "Erokan";
+console.log("Merhaba benim adım :" + isim);
+
+function toplama(sayi1, sayi2, sayi3){
+    return sayi1 + sayi2
+}
+
+console.log(toplama(10,20,30));
+console.log(toplama(100,200));
+
