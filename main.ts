04d6dbd7a82cc12e2663d469884bd874644fc0e1
Ali Mert <mail@alimert.net>
2020-02-21 22:02:37 +0300

    main.ts , package.json fixs
    main ts prettified, package json better description

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-function topla(sayi1:number, sayi2:number):number{
-    return sayi1 + sayi2
+function topla(sayi1: number, sayi2: number): number {
+  return sayi1 + sayi2;
 }
 
-console.log(topla(10,20));
-console.log(topla(100,200));
+console.log(topla(10, 20));
+console.log(topla(100, 200));
 
-var isim = "Erokan"
+var isim = "Erokan";
